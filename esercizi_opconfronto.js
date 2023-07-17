2ffe37d0c6287d9e7a6eee1e1396ca2f80d77e7c
Gabriele Iacono <gabry20035@yahoo.it>
2023-07-17 20:43:00 +0200

    completed comparison exercise

---
+++ esercizi_opconfronto.js
@@ -0,0 +1,47 @@
+// Chiedere all'utente di inserire due stringhe e verificare se sono uguali.
+
+const str1 = "mario";
+const str2 = "mario";
+
+if (str1 === str2) {
+  console.log("uguali");
+}
+
+// Chiedere all'utente di inserire due numeri e determinare il minore tra di essi.
+
+const num1 = 4;
+const num2 = 3;
+
+if (num1 < num2) {
+  console.log("il minore è num1 con il valore " + num1);
+} else {
+  console.log("il minore è num2 con il valor " + num2);
+}
+
+// Chiedere all'utente di inserire due numeri e verificare se il primo numero è maggiore o uguale del secondo.
+
+const num3 = 5;
+const num4 = 10;
+
+if (num3 >= num4) {
+  console.log("num3 è maggiore o uguale di num4");
+}
+
+// Date due stringhe che rappresentano dei nomi di due utenti controllare se hanno lo stesso nome.
+
+const username1 = "luigi";
+const username2 = "luigi";
+
+if (username1 === username2) {
+  console.log("i nomi degli utenti sono uguali");
+}
+
+// Dato un numero controllare se il suo valore esiste, se esiste, stampare in console il valore esiste o se non esiste stampare non esiste IN CONSOLE
+
+let num5 = 5;
+
+if (num5) {
+  console.log("esiste");
+} else {
+  console.log("non esiste");
+}
