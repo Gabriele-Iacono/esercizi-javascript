c6db852bc1fab8d6e34c1df2f416efd5dba102b0
Gabriele Iacono <gabry20035@yahoo.it>
2023-07-24 13:18:04 +0200

    completed exercise

---
+++ esercizi_ciclofor.js
@@ -0,0 +1,20 @@
+//Scrivi un programma che utilizzi un ciclo for per stampare sulla console i numeri da 1 a 10.
+
+for(let i = 1; i < 11; i=i+1){
+    console.log(i);
+}
+
+//Utilizza un ciclo for per calcolare la somma dei numeri da 1 a 100 e stampa il risultato.
+
+let somma = 0;
+for(let i=1; i <101; i=i+1){
+somma = i+somma 
+}
+console.log(somma)
+
+//Scrivi un programma che dato in input un numero stampi la tabellina delle suo numero
+
+let numero = 2  //cambierà solo il numero della moltiplicazione perchè la variabile non la sto cambiando
+for (let nmoltiplicazione = 1; nmoltiplicazione<11; nmoltiplicazione=nmoltiplicazione+1){
+console.log(numero*nmoltiplicazione)
+}
